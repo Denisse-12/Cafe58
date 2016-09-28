37666280c0ccf6434a2ba3b72e70d37b3bb1a411
xitlalli98 <reginar98@hotmail.com>
2016-09-27 18:47:58 -0700

    Usuario

---
+++ AppTemplate/AppTemplate/App.Entities/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Usuario
+    {
+        [key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNac { get; set; }
+        public string TipoUsuario { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
