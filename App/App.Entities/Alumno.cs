800344c17c951215ca3ad6b9364fb87d43153cc2
Denisse-12 <denisse.4567@outlook.com>
2016-09-27 18:53:19 -0700

    purgando

--- App/App.Entities/Alumno.cs
+++ App/App.Entities/Alumno.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Entities
 {
     public class Alumno
-    {
-        public int ID { get; set; }
+    {   
+        [Key]
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public int  NoControl { get; set; }
