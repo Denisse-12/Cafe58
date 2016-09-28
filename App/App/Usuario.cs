5931a1d2ba6ba73761f0f4c8a04f2224f0393b47
Xitlalli Ruiz <Xitlalli Ruiz>
2016-09-27 23:33:49 -0700

    usuario

---
+++ App/App/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App
+{
+    public class Usuario
+    {
+        [key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime FechaNacimento { get; set; }
+        public string TipoUsuario { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
