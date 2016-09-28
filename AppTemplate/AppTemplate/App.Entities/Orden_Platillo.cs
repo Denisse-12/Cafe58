35c5c664c0f5bb5133843aeae16cae33e7bc563b
xitlalli98 <reginar98@hotmail.com>
2016-09-27 19:20:36 -0700

    Ordenplatillo

---
+++ AppTemplate/AppTemplate/App.Entities/Orden_Platillo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Orden_Platillo
+    {
+    [key]
+        public int Id { get; set; }
+        public int Id_Orden { get; set; }
+        public int Id_Platillo { get; set; }
+    }
+}
