f364b0974dc8f5f3a330af096e4bc5f76c2bfb55
ManuelMJ10 <manuelmartinezjimenez2@gmail.com>
2016-09-28 18:38:10 -0700

    Menuplatillo actualizada

--- AppTemplate/AppTemplate/App.Entities/MenuPlatillo.cs
+++ AppTemplate/AppTemplate/App.Entities/MenuPlatillo.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Entities
 {
     class Menuplatillo
     {
+        [Key]
+        public int Id { get; set; }
+        public int IdPlatillo { get; set; }
+        public int IdMenu { get; set; }
+
     }
 }
