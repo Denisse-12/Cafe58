19312d8ce722bd8c723e8dd43aa708728648f24d
ManuelMJ10 <manuelmartinezjimenez2@gmail.com>
2016-09-28 18:07:49 -0700

    entidad actualizada platillo

--- App/App.Entities/Platillo.cs
+++ App/App.Entities/Platillo.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Entities
 {
-    class Platillo
+    public class Platillo
     {
+        [Key]
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public byte Imagen { get; set; }
+        public double Numero { get; set; }
+        public Boolean Beca { get; set; }
+
     }
 }
