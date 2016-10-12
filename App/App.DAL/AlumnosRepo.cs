485db0517f246583451c74755179d25804813b40
Denisse-12 <denisse.4567@outlook.com>
2016-10-11 19:24:41 -0700

    Repositorio Alumno

--- App/App.DAL/AlumnosRepo.cs
+++ App/App.DAL/AlumnosRepo.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App.Entities;
 
 namespace App.DAL
 {
     class AlumnosRepo
     {
+
+        private AppDBContext _Contexto ;
+
+        public AlumnosRepo()
+        {
+
+            _Contexto = new AppDBContext();
+        }
+
+        public void Agregar (Alumno alumno)
+    {
+        _Contexto.Alumnos.Add(alumno);
+        _Contexto.SaveChanges();
+
+    }
+
+        public void Borrar(Alumno alumno)
+        {
+            _Contexto.Alumnos.Remove(alumno);
+            _Contexto.SaveChanges();
+
+        }
+
+
     }
 }
