945893042e19cd9bfdf6a40921f8ac4eacfe3ef9
manuel martinez jimenez <manuel martinez jimenez>
2016-10-17 16:33:45 -0700

    Ingredientes Repo

---
+++ App/App.DAL/IngredientesRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Entities;
+using App.DAL;
+
+
+namespace App.BLL
+{
+     public class IngredientesRepo
+
+    {
+         private AppDBContext _Contexto;
+
+        public IngredientesRepo()
+        {
+           _Contexto = new AppDBContext();
+        }
+
+        public void Agregar(Ingredientes ingredientes)
+        {
+            _Contexto.Ingredientes.Add(ingredientes);
+            _Contexto.SaveChanges();
+        }
+
+
+        public void Borrar (Ingredientes ingredientes)
+        {
+            _Contexto.Ingredientes.Remove(ingredientes);
+            _Contexto.SaveChanges();
+        }
+
+        public List<Ingredientes> TraerTodo()
+        {
+            return _Contexto.Ingredientes.OrderBy(x => x.Id).ToList();
+        }
+
+        public Ingredientes TraerPorId(int id)
+        {
+            return _Contexto.Ingredientes.FirstOrDefault(x => x.Id == id);
+        }
+ 
+    }
+}
---
+++ App/App.DAL/MenuPlatilloRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Entities;
+using App.DAL;
+
+
+namespace App.BLL
+{
+    public class MenuPlatilloRepo
+    {
+        private AppDBContext _Contexto;
+
+        public MenuPlatilloRepo()
+        {
+           _Contexto = new AppDBContext();
+        }
+
+        public void Agregar (MenuPlatillo MenuPlatillo)
+        {
+            _Contexto.MenuPlatillo.Add(MenuPlatillo);
+            _Contexto.SaveChanges();
+        }
+
+
+        public void Borrar (MenuPlatillo MenuPlatillo)
+        {
+            _Contexto.MenuPlatillo.Remove(MenuPlatillo);
+            _Contexto.SaveChanges();
+        }
+
+        public List<MenuPlatillo> TraerTodo()
+        {
+            return _Contexto.MenuPlatillo.OrderBy(x => x.Id).ToList();
+        }
+
+        public MenuPlatillo TraerPorId(int id)
+        {
+            return _Contexto.MenuPlatillo.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
