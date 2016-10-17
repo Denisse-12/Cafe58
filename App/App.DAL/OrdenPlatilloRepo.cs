9cdcb9672efb186fd4c6dce07b5bebda3ebf2c8c
Xitlalli Ruiz <Xitlalli Ruiz>
2016-10-17 13:37:49 -0700

    OrdenPlatilloRepo

---
+++ App/App.DAL/OrdenPlatilloRepo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Entities;
+
+namespace App.DAL
+{
+    public class OrdenPlatilloRepo
+    {
+        private AppDBContext _Contexto;
+
+        public OrdenPlatilloRepo()
+        {
+
+            _Contexto = new AppDBContext();
+        }
+
+        public void Agregar(OrdenPlatillo ordenplatillo)
+        {
+            _Contexto.OrdenesPlatillos.Add(ordenplatillo);
+            _Contexto.SaveChanges();
+
+        }
+
+        public void Borrar(OrdenPlatillo ordenplatillo)
+        {
+            _Contexto.OrdenesPlatillos.Remove(ordenplatillo);
+            _Contexto.SaveChanges();
+
+        }
+
+        public List<OrdenPlatillo> TraerTodo()
+        {
+            return _Contexto.OrdenesPlatillos.OrderBy(x => x.Id).ToList();
+
+        }
+
+        public OrdenPlatillo TraerPorId(int id)
+        {
+            return _Contexto.OrdenesPlatillos.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
